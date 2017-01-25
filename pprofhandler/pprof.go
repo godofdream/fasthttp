09faad794b32fcd082761ed351838c2568bc3184
Erik Dubbelboer <erik@dubbelboer.com>
2017-01-25 06:39:02 +0000

    Merge Atomx changes

--- pprofhandler/pprof.go
+++ pprofhandler/pprof.go
@@ -8,28 +8,20 @@ import (
 	"github.com/godofdream/fasthttp/fasthttpadaptor"
 )
 
-var (
-	cmdline = fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Cmdline)
-	profile = fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Profile)
-	symbol  = fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Symbol)
-	trace   = fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Trace)
-	index   = fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Index)
-)
-
 // PprofHandler serves server runtime profiling data in the format expected by the pprof visualization tool.
 //
 // See https://golang.org/pkg/net/http/pprof/ for details.
 func PprofHandler(ctx *fasthttp.RequestCtx) {
 	ctx.Response.Header.Set("Content-Type", "text/html")
 	if strings.HasPrefix(string(ctx.Path()), "/debug/pprof/cmdline") {
-		cmdline(ctx)
+		fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Cmdline)(ctx)
 	} else if strings.HasPrefix(string(ctx.Path()), "/debug/pprof/profile") {
-		profile(ctx)
+		fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Profile)(ctx)
 	} else if strings.HasPrefix(string(ctx.Path()), "/debug/pprof/symbol") {
-		symbol(ctx)
+		fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Symbol)(ctx)
 	} else if strings.HasPrefix(string(ctx.Path()), "/debug/pprof/trace") {
-		trace(ctx)
+		fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Trace)(ctx)
 	} else {
-		index(ctx)
+		fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Index)(ctx)
 	}
 }
